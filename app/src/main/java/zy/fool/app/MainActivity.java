3a5132641cf95eb71267a00a5e75bb15b4737868
David Lau <zhiyong.liu@aliyun.com>
2014-02-28 16:30:50 +0800

    change layout

--- app/src/main/java/zy/fool/app/MainActivity.java
+++ app/src/main/java/zy/fool/app/MainActivity.java
@@ -1,65 +1,63 @@
 package zy.fool.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    LittleFoolAdapter mFoolAdapter ;
+    SubFoolView mView ;
+    String [] mStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+        View emptyView = findViewById(android.R.id.empty);
+        mView = (SubFoolView)findViewById(R.id.foolview);
+        mFoolAdapter = new LittleFoolAdapter(this);
+        mView.setEmptyView(emptyView);
+        //mView.setOnPullListener(this);
+        //mFoolView.setAdapter(mFoolAdapter);
 
+        mView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, GENRES));
+        //mView.setPullation(2);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
+    private static final String[] GENRES = new String[] {
+            "-----0-----",
+            "-----1-----",
+            "-----2-----",
+            "-----3-----",
+            "-----4-----",
+            "-----5-----",
+            "-----6-----",
+            "-----7-----",
+            "-----8-----",
+            "-----9-----",
+            "-----10-----",
+            "-----11-----",
+            "-----13-----",
+            "-----14-----",
+            "-----15-----",
+            "-----16-----",
+
+
+    };
+
+
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public boolean onPullOut() {
+        // TODO Auto-generated method stub
+        return true;
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-
 }
